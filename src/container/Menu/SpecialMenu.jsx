b4b7f57096ff74f9dd76f7c53183a861b9cd4122
zainab66 <zainab66r@gmail.com>
2023-05-17 11:23:09 -0400

    add imports to SpecialMenu component

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { images, data } from '../../constants';
+import { SubHeading, MenuItem } from '../../components';
+
+import './SpecialMenu.css';
 
 export default function SpecialMenu() {
   return <div>SpecialMenu</div>;
