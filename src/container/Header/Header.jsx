bf71d4c27709d465448bcf336167e3a1ddc53bdf
zainab66 <zainab66r@gmail.com>
2023-05-17 10:18:32 -0400

    pass title from Header as props to SubHeading to render title dynamically

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,8 @@ export default function Header() {
   return (
     <div className="app__header app__wrapper section__padding" id="home">
       <div className="app__wrapper_info">
-        <SubHeading />
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
       </div>
       <div className="app__wrapper_img"></div>
     </div>
