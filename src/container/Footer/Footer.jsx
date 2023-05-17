4951e2fd0060b40559d06704000920021e4144c6
zainab66 <zainab66r@gmail.com>
2023-05-17 13:10:24 -0400

    add css stlying to the footer to space items, add color and hocer effects for icons

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -38,10 +38,10 @@ export default function Footer() {
         </div>
         <div className="app__footer-links_work">
           <h1 className="app__footer-headtext">Working Hours</h1>
-          <p className="p__opensans">Monday - Friday:</p>
-          <p className="p__opensans">08:00 am -12:00 am</p>
-          <p className="p__opensans">Saturday / Sunday:</p>
-          <p className="p__opensans">08:30am -11:00 pm</p>
+          <p className="p__opensans">Monday - Thursday:</p>
+          <p className="p__opensans">08:00 am -11:00 am</p>
+          <p className="p__opensans">Friday - Sunday:</p>
+          <p className="p__opensans">08:30am -12:00 pm</p>
         </div>
       </div>
       <div className="footer__copyright">
