5a0c00719e43fb079813c3ea342b5fdff71e9142
zainab66 <zainab66r@gmail.com>
2023-05-16 21:15:09 -0400

    add imports to Navbar file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from './container';
+import { Navbar } from './components';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { GiHamburger } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
+import './Navbar.css';
 
 export default function Navbar() {
   return <div>Navbar</div>;
