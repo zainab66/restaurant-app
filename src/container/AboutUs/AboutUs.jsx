aaf3cd72f874dd0f123a0a3432ea4ebfe5626e23
zainab66 <zainab66r@gmail.com>
2023-05-17 11:14:38 -0400

    add knife image and duplicate about us for history section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -24,6 +24,21 @@ export default function AboutUs() {
             Know More
           </button>
         </div>
+        <div className="app__aboutus-content_knife flex__center">
+          <img src={images.knife} alt="about_knife" />
+        </div>
+        <div className="app__aboutus-content_history">
+          <h1 className="headtext__cormorant">Our History</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </div>
       </div>
     </div>
   );
