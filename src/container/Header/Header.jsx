af3add5c050847fd7ad661dc7b8a4ade6f290307
zainab66 <zainab66r@gmail.com>
2023-05-17 10:09:58 -0400

    import Subheading and add to code

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 import './Header.css';
 
 export default function Header() {
   return (
     <div className="app__header app__wrapper section__padding" id="home">
-      <div className="app__wrapper_info"></div>
+      <div className="app__wrapper_info">
+        <SubHeading />
+      </div>
       <div className="app__wrapper_img"></div>
     </div>
   );
