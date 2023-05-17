cd7adb51d9711c24f4bbf5ed95a64052312c01f8
zainab66 <zainab66r@gmail.com>
2023-05-16 21:19:44 -0400

    add login and booking sections to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,6 +27,15 @@ export default function Navbar() {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Login / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book A Table
+        </a>
+      </div>
     </div>
   );
 }
