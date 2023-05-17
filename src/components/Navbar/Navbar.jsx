499a4ebdf886443b579aa030c6eed86f8b354a08
zainab66 <zainab66r@gmail.com>
2023-05-16 21:23:02 -0400

    add links to smallscreen section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { GiHamburger } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
+
 import './Navbar.css';
 
 export default function Navbar() {
   return (
-    <div className="app__navbar">
+    <nav className="app__navbar">
       <div className="app__navbar-logo">
         <img src={images.gericht} alt="app logo" />
       </div>
@@ -36,6 +37,34 @@ export default function Navbar() {
           Book A Table
         </a>
       </div>
-    </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+          <MdOutlineRestaurantMenu
+            fontSize={27}
+            className="overlay__close"
+            onClick={() => {}}
+          />
+          <ul className="app__navbar-smallscreen-links">
+            <li className="p__opensans">
+              <a href="#home">Home</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#about">About</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#menu">Menu</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#awards">Awards</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
