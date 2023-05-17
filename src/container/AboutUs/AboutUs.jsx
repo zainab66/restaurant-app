7501e08e7c9a742d800742ce13f231fc2c35eef6
zainab66 <zainab66r@gmail.com>
2023-05-17 11:12:21 -0400

    add background logo and about us section with text and in-line styling

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { images } from '../../constants';
+import './AboutUs.css';
 
 export default function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div
+      className="app__aboutus app__bg flex__center section__padding"
+      id="about"
+    >
+      <div className="app__aboutus-overlay flex__center">
+        <img src={images.G} alt="G letter" />
+      </div>
+      <div className="app__aboutus-content flex-center">
+        <div className="app__aboutus-content_about">
+          <h1 className="headtext__cormorant">About us</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
