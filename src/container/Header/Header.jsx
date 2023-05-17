c12c48bf488e3ffbbe8e8247e0d3ed3bb2b7e3ee
zainab66 <zainab66r@gmail.com>
2023-05-17 10:08:36 -0400

    add imports and initial divs with classNames

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { images } from '../../constants';
+
+import './Header.css';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info"></div>
+      <div className="app__wrapper_img"></div>
+    </div>
+  );
 }
