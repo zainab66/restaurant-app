afc1f6c7913e83c1792cd7b73fce04b1381cc89a
zainab66 <zainab66r@gmail.com>
2023-05-17 11:26:51 -0400

    add images and duplicate wine map replacing for cocktail data

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -20,6 +20,17 @@ export default function SpecialMenu() {
             ))}
           </div>
         </div>
+        <div className="app__specialMenu-menu_img">
+          <img src={images.menu} alt="menu images" />
+        </div>
+        <div className="app__specialMenu-menu_cocktails flex__center">
+          <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+          <div className="app__specialMenu_menu_items">
+            {data.cocktails.map((cocktail, index) => (
+              <p>{cocktail.title}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
