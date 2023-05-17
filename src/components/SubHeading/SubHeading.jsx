9782ebb111d78f0b66c82efe9ee0434990c26b8a
zainab66 <zainab66r@gmail.com>
2023-05-17 10:11:57 -0400

    import items to subheading and add styling to div

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { images } from '../../constants';
 
 export default function SubHeading() {
-  return <div>SubHeading</div>;
+  return (
+    <div style={{ marginBottom: '1rem' }}>
+      <p className="p__cormorant">TITLE</p>
+      <img src={images.spoon} alt="spoon" className="spoon__img" />
+    </div>
+  );
 }
